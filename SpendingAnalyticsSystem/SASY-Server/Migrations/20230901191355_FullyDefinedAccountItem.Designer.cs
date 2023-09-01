7ad94a7ebe5ab9cf97828d45e021ee04369fa48d
richardaiki <richard.imrie@sky.com>
2023-09-01 20:23:07 +0100

    commit again ef working

--- SpendingAnalyticsSystem/SASY-Server/Migrations/20230901191355_FullyDefinedAccountItem.Designer.cs
+++ SpendingAnalyticsSystem/SASY-Server/Migrations/20230901191355_FullyDefinedAccountItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpendingAnalyticsSystem.API;
 
@@ -11,9 +12,10 @@ using SpendingAnalyticsSystem.API;
 namespace SpendingAnalyticsSystem.API.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20230901191355_FullyDefinedAccountItem")]
+    partial class FullyDefinedAccountItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SpendingAnalyticsSystem/SASY-Server/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace SpendingAnalyticsSystem.API.Models
+{
+    public enum Subcategory
+    {
+        DirectDebit,
+        CashWithdrawl,
+        CardPurchase,
+        StandingOrder,
+        ContactlessCardPurchase,
+        FundsTransfer,
+        CounterCredit
+    }
+}
